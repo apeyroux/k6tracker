05079f5003570bda53a70c1ff4453c696a83891a
Alexandre Peyroux <alex@xn--wxa.email>
2018-04-14 20:45:48 +0200

    check status

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.Wreq
-import Control.Lens
-import Data.Aeson.Lens
+import           Control.Concurrent
+import           Control.Lens
+import           Data.Aeson
+import           Data.Aeson.Lens
 import qualified Data.Text as T
-import Data.Aeson
+import           Network.Wreq
 
+data K6Status = K6Status {
+  k6Av :: T.Text
+  , k6Ds :: T.Text
+  } deriving Show
+
+type Hardware = String
+
+ovhws :: Hardware -> String
+ovhws hardware = "https://www.ovh.com/engine/api/dedicated/server/availabilities?country=fr&&hardware=" ++ hardware
+
+hardstatus :: Hardware -> IO [K6Status]
+hardstatus h = do
+  r <- get (ovhws h)
+  return $ r ^.. responseBody
+    . values
+    . key "datacenters"
+    . values
+    . to (\e -> K6Status (e ^. key "availability" . _String) (e ^. key "datacenter" . _String))
+    . filtered (\k6 -> k6Av k6 == "available")
 
 main :: IO ()
-main = do
-  r <- get ovhws
-  print $ r ^. responseBody . key "hardware" . _String
+main = putStrLn ("checking " ++ ovhws h ++ "...")
+       >> hardstatus h
+       >>= mapM_ (\ds -> putStrLn $ (T.unpack (k6Av ds)) ++ " at " ++ (T.unpack (k6Ds ds)))
+       >> threadDelay delay
+       >> main
   where
-    ovhws = "https://www.ovh.com/engine/api/dedicated/server/availabilities?country=fr"
+    h = "1801sk06"
+    delay = 1000000 * 10 -- 10 sec
